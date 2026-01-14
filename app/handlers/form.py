25cc2b7504b41792ffdaba585be094413aecf06a
Iulian <gvazuk@gmail.com>
2026-01-14 18:52:23 +0200

    start working with docker

--- app/handlers/form.py
+++ app/handlers/form.py
@@ -3,7 +3,7 @@ from aiogram import Router
 from aiogram.fsm.context import FSMContext
 
 from app.states import VideoFlow
-from app.handlers.video import videos
+from app.handlers.video import video_data
 from app.keyboards.inline import next_video_kb
 
 router = Router()
@@ -14,17 +14,18 @@ async def user_phone_handler(message, state):
         phone = message.contact.phone_number
         first_name = message.contact.first_name or ""
         last_name = message.contact.last_name or ""
-        name = f"{first_name}{last_name}".strip()
+        name = f"{first_name} {last_name}".strip()
         await state.update_data(phone=phone, name=name)
         await message.answer("Vă mulţumim, acum putem continua")
 
         await state.set_state(VideoFlow.watching_videos)
         await state.update_data(video_index=0)
 
-        first_video_id = videos[0]
+        first_video = video_data[0]
 
         await message.answer_video(
-            video=first_video_id,
+            video=first_video["id"],
+            caption=first_video["caption"],
             reply_markup=next_video_kb()
         )
 
--- app/handlers/video.py
+++ app/handlers/video.py
@@ -8,7 +8,41 @@ from app.keyboards.inline import next_video_kb, final_kb
 
 router = Router()
 
-videos = [VIDEO_1, VIDEO_2, VIDEO_3, VIDEO_4]
+video_data = [
+    {
+        "id": VIDEO_1,
+        "caption": (
+            "De ce majoritatea antreprenorilor muncesc mult, învață constant\n"
+            "și totuși rămân sub potențialul lor real.\n"
+            "Acest video pune baza pentru tot ce urmează.\n"
+            "Urmărește-l până la capăt."
+        )
+    },
+    {
+        "id":VIDEO_2,
+        "caption": (
+            "Dacă ai recunoscut ceva din tine în acest video, este normal.\n"
+            "Problema NU este lipsa de disciplină sau de inteligență.\n"
+            "Există un mecanism mai profund care se activează exact când apare creșterea.\n"
+            "▶️ Video 2 explică de ce, pentru mulți oameni, banii pot deveni „pericol” la nivel subconștient"
+        )
+    },
+    {
+        "id": VIDEO_3,
+        "caption": (
+            "Dacă acest video a atins un punct sensibil, este un semn bun.\n"
+            "Multe dintre aceste programe nu sunt create de tine,\n"
+            "ci preluate din familie, copilărie sau contexte vechi.\n"
+            "▶️ Video 3 vorbim despre emoțiile primare care mențin aceste blocaje active."
+        )
+    },
+    {
+        "id": VIDEO_4,
+        "caption": (
+            "▶️ Video 4 explică de ce starea interioară este factorul principal și ce se schimbă atunci când blocajele sunt eliminate."
+        )
+    }
+]
 
 @router.callback_query(
     VideoFlow.watching_videos,
@@ -18,10 +52,12 @@ async def next_video(callback, state):
     data = await state.get_data()
     video_index = data.get("video_index", 0)
     video_index += 1
-    if video_index < len(videos):
+    if video_index < len(video_data):
         await state.update_data(video_index=video_index)
+        current_video = video_data[video_index]
         await callback.message.answer_video(
-            video=videos[video_index],
+            video=current_video["id"],
+            caption=current_video["caption"],
             reply_markup=next_video_kb()
         )
     else:
